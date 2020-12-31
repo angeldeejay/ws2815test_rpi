4a6582fb855afb4262f5dfb6bdd7961202fdc304
Andres Vanegas <ukab72106@gmail.com>
2020-12-30 22:22:27 -0500

    Improved workaround

--- app/lib/pyledshop/WifiLedShopLight.py
+++ app/lib/pyledshop/WifiLedShopLight.py
@@ -216,14 +216,14 @@ class WifiLedShopLight:
                 self.sock.sendall(raw_data)
                 break
             except (socket.timeout, BrokenPipeError) as e:
-                self.__log('Error sending data: %s' % str(e))
+                # self.__log('Error sending data: %s' % str(e))
                 if (attempts < self.retries):
                     self.reconnect()
                     attempts += 1
                 else:
                     break
             except Exception as e:
-                self.__log('Error sending data: %s' % str(e))
+                # self.__log('Error sending data: %s' % str(e))
                 pass
             time.sleep(1)
 
