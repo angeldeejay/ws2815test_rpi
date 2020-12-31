24e33cc4b3494642364187dc655ad2507b07c02e
Andres Vanegas <ukab72106@gmail.com>
2020-12-30 23:39:46 -0500

    Improved workaround

--- app/lib/network_scanner.py
+++ app/lib/network_scanner.py
@@ -78,7 +78,6 @@ class NetworkScanner:
         """Pings hosts in queue"""
         while True:
             if not self.__running:
-                time.sleep(1)
                 break
             (ip, ip_status) = self.__in_queue.get()
             try:
