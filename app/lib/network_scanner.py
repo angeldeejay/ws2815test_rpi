56f1c60f897542b344ec2c2d8680aa5d28d92ce1
Andres Vanegas <ukab72106@gmail.com>
2020-12-31 00:14:06 -0500

    Improved workaround

--- app/lib/network_scanner.py
+++ app/lib/network_scanner.py
@@ -39,7 +39,7 @@ class Thread(threading.Thread):
             return self._thread_id
 
         # no, look for it in the _active dict
-        active_threads = threading._active.items()
+        active_threads = threading._active.items().copy()
         for thread_id, thread_object in active_threads:
             if thread_object is self:
                 self._thread_id = thread_id
