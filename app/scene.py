b29d6b05c6d6bd43f8b069cf9aa7d0d0ffe195d2
Andres Vanegas <ukab72106@gmail.com>
2021-01-19 20:54:35 -0500

    Unified services

--- app/scene.py
+++ app/scene.py
@@ -1,9 +1,13 @@
-import time
-from lib.utils import evaluate_day_night
-from lib.pyledshop import WifiLedShopLight
+from lib.animations import NewKITT, RainbowCycle, shutdown as shutdownPixels
 from lib.devices import Sonoff
 from lib.network_scanner import NetworkScanner
+from lib.pyledshop import WifiLedShopLight
+from lib.utils import evaluate_day_night
 from sys import argv
+import board
+import neopixel
+from threading import Thread
+import time
 
 scanner = NetworkScanner()
 
@@ -31,13 +35,52 @@ log(f'Sonoff detected!')
 
 sonoff = Sonoff(broker=sonoff_broker, device="desktop")
 controller = None
+pixels = None
+night_mode = False
 
+def load_pixels():
+    global pixels
+    if pixels is None:
+        try:
+            pixels = neopixel.NeoPixel(board.D18, 11, brightness=1.0, auto_write=False, pixel_order=neopixel.GRB)
+        except:
+            pixels = None
+            pass
+
+def unload_pixels():
+    global pixels
+    global fan_thread
+    fan_thread.join(0)
+    load_pixels()
+    if pixels is not None:
+        try:
+            shutdownPixels(pixels)
+            pixels.deinit()
+        except:
+            pass
+
+def pixels_animation(scanner, night_mode, main_host):
+    while True:
+        load_pixels()
+        try:
+            if scanner.is_alive(main_host):
+                if night_mode:
+                    RainbowCycle(pixels, 0.001, 1)
+                else:
+                    NewKITT(pixels, 255, 0, 0, 1, 0.075, 0, 1)
+        except:
+            pass
+
+fan_thread = Thread(target=pixels_animation, args=[scanner, night_mode, main_host])
+fan_thread.setDaemon(True)
+fan_thread.start()
 
 def shutdown():
     global controller
     global scanner
     global sonoff
     print(f'\n{__name__}', 'Exiting...')
+    unload_pixels()
     attempts = 0
     while not sonoff.connected:
         if attempts < 10:
@@ -72,7 +115,8 @@ else:
         while True:
             preset = None
             speed = None
-
+            night_mode = evaluate_day_night(start_at, end_at, date_fmt, time_fmt)
+            # Scene lights
             if sonoff.connected:
                 if scanner.is_alive(main_host):
                     if not sonoff.on:
@@ -93,7 +137,7 @@ else:
                         controller.set_segments(1)
                         controller.set_lights_per_segment(144)
 
-                    if evaluate_day_night(start_at, end_at, date_fmt, time_fmt):
+                    if night_mode:
                         if controller.state.mode != 0:
                             log('Activating night mode!')
                             controller.set_preset(0)
