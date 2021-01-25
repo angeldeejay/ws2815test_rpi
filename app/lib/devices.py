b6959963803d84b641aff6256c68bfc79435783a
Andres Vanegas <ukab72106@gmail.com>
2021-01-24 22:39:37 -0500

    Simulations

--- app/lib/devices.py
+++ app/lib/devices.py
@@ -1,10 +1,95 @@
-import random
-import time
-import socket
-import binascii
 from paho.mqtt import client as mqtt_client
-import threading
+from rpi_ws281x import PixelStrip, Color
 import asyncore
+import binascii
+import random
+import socket
+import threading
+import time
+
+class LedStrip:
+    def __init__(self, name, gpio_pin=None, led_count=None, reverse=None, interval=None, frequency=None):
+        self.animation = None
+        self.name = name
+        self.gpio_pin = int(gpio_pin)
+        self.led_count = int(led_count)
+        self.frequency = int(self.__default_value(frequency, 800000))
+        self.dma = 10
+        self.brightness = 30
+        self.interval = float(self.__default_value(interval, 0.05))
+        self.reverse = self.__default_value(reverse, False)
+        self.channel = 1 if gpio_pin in [13, 19, 41, 45, 53] else 0
+        self.thread = None
+        self.thread_active = False
+        self.led_strip = PixelStrip(
+            self.led_count, self.gpio_pin, self.frequency, self.dma, False, self.brightness, self.channel)
+
+    def set_animation(self, animation_fn):
+        self.animation = self.__default_value(animation_fn, self.__lambda)
+
+    def set_pixel_color(self, i, color):
+        try:
+            self.led_strip.setPixelColor(i, color)
+        except:
+            pass
+
+    def show(self):
+        try:
+            self.led_strip.show()
+            True
+        except:
+            pass
+
+    def begin(self):
+        try:
+            self.led_strip.begin()
+            True
+        except:
+            error_output = "\n\n\t" + format_exc().replace("\n", "\n\t")
+            self.log("\n\t" + error_output + "\n", "error")
+            pass
+
+    def __lambda(self, instance):
+        while True:
+            if not instance.thread_active:
+                break
+            sleep(instance.interval)
+
+    def __default_value(self, value, default=None):
+        return default if value is None else value
+
+    def start(self):
+        if self.animation is not None:
+            self.thread_active = True
+            self.thread = Thread(target=self.animation,
+                                 daemon=True, args=[self])
+            self.log("Starting thread")
+            self.thread.start()
+
+    def stop(self):
+        self.thread_active = False
+        if self.thread is not None:
+            self.log("Stopping thread")
+            while self.thread.is_alive():
+                sleep(0.01)
+
+        try:
+            for i in range(0, self.led_count):
+                self.set_pixel_color(i, Color(0, 0, 0))
+            self.show()
+        except:
+            pass
+
+        self.thread = None
+        self.changed = False
+        self.log("Shutted down")
+
+    def log(self, message, level="debug"):
+        print(str('[{:^7}]'.format(level.upper())) +
+              ' {' + str(self.name) + "}: " + str(message))
+
+    def __repr__(self):
+        return self.__class__.__name__ + str(vars(self))
 
 
 class Sonoff:
@@ -40,7 +125,13 @@ class Sonoff:
         self.on = status == "ON"
 
     def __on_disconnect(self, client, userdata, rc):
+        self.connected = False
         self.__log('Disconnected. Reconnecting...')
+        try:
+            self.client.loop_stop()
+        except:
+            pass
+        self.__connect()
 
     def __connect(self):
         self.client = mqtt_client.Client(self.client_id)
